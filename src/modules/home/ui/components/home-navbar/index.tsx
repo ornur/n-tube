2274f11941f9a8641edfe5106d1738c96f90ba2d
ornur <nurdauletaidaruli@gmail.com>
2025-07-07 21:44:02 +0500

    Refactor HomeNavBar component to reorder imports and improve structure

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -1,6 +1,9 @@
-import { SidebarTrigger } from "@/components/ui/sidebar"
 import Link from "next/link"
 import Image from "next/image"
+
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import { AuthButton } from "@/modules/auth/auth-button"
+import { SearchInput } from "./seach-input"
 
 export const HomeNavBar = () => {
     return (
@@ -19,6 +22,14 @@ export const HomeNavBar = () => {
                             <p className="text-xl font-semibold tracking-tight">NTube</p>
                         </div>
                     </Link>
+                </div>
+
+                <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+                    <SearchInput />
+                </div>
+
+                <div className="flex-shrink-0 items-center flex gap-4">
+                    <AuthButton />
                 </div>
             </div>
         </nav>
