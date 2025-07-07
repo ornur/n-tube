e50b15f904fb27e4d00a17ea510896308703556d
ornur <nurdauletaidaruli@gmail.com>
2025-07-07 22:16:50 +0500

    Refactor AuthButton component to include user authentication states with SignedIn and SignedOut

--- src/modules/auth/auth-button.tsx
+++ src/modules/auth/auth-button.tsx
@@ -1,14 +1,27 @@
+"use client";
+
 import { UserCircleIcon } from "lucide-react"
+import { UserButton, SignInButton, SignOutButton, SignedIn, SignedOut } from "@clerk/nextjs"
+
 import { Button } from "@/components/ui/button"
 
 export const AuthButton = () => {
   return (
-    <Button
-      variant="outline"
-      className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/12 rounded-full shadow-none"
-    >
-      <UserCircleIcon />
-      Sign in
-    </Button>
+    <>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button
+            variant="outline"
+            className="px-4 py-2 text-sm font-medium text-blue-600 hover:text-blue-500 border-blue-500/12 rounded-full shadow-none"
+          >
+            <UserCircleIcon />
+            Sign in
+          </Button>
+        </SignInButton>
+      </SignedOut>
+    </>
   )
 }
