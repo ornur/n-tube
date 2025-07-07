59dca5764bac89713dcb26452ee5c963bf230b83
ornur <nurdauletaidaruli@gmail.com>
2025-07-07 21:44:23 +0500

    Refactor HomeLayout component to include HomeSidebar and improve layout structure

--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,5 +1,6 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { HomeNavBar } from "../components/home-navbar";
+import { HomeSidebar } from "../components/home-sidebar";
 
 interface HomeLayoutProps {
     children: React.ReactNode;
@@ -10,7 +11,12 @@ export const HomeLayout = ({ children }: HomeLayoutProps) => {
         <SidebarProvider>
             <div className="w-full">
                 <HomeNavBar />
-                {children}
+                <div className="flex min-h-screen pt-[4rem]">
+                    <HomeSidebar />
+                    <main className="flex-1 overflow-y-auto">
+                        {children}
+                    </main>
+                </div>
             </div>
         </SidebarProvider>
     )
