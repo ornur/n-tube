705cf33cc76109d72028cc1320dc0b49fc346d9d
ornur <nurdauletaidaruli@gmail.com>
2025-07-07 19:24:45 +0500

    Add HomeNavBar component with SidebarTrigger and logo

---
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -0,0 +1,26 @@
+import { SidebarTrigger } from "@/components/ui/sidebar"
+import Link from "next/link"
+import Image from "next/image"
+
+export const HomeNavBar = () => {
+    return (
+        <nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+            <div className="flex items-center gap-4 w-full">
+                <div className="flex items-center flex-shrink-0">
+                    <SidebarTrigger />
+                    <Link href="/">
+                        <div className="flex p-4 items-center gap-1">
+                            <Image
+                                src="/logo.svg"
+                                alt="N-Tube Logo"
+                                width={32}
+                                height={32}
+                            />
+                            <p className="text-xl font-semibold tracking-tight">NTube</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
