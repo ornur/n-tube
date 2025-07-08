202f35724ff35d7abc8589d29182bb5027374bcd
ornur <nurdauletaidaruli@gmail.com>
2025-07-08 23:30:08 +0500

    feat: implement TRPC prefetching and error handling in Home component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <p className="text-red-500">I will load videos in the future</p>;
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Hello from TRPC!" });
+  return (
+    <HydrateClient>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ErrorBoundary fallback={<div>Something went wrong!</div>}  >
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
