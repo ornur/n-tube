1594f6aa3d179434626ca37ff2032f0ce8294152
ornur <nurdauletaidaruli@gmail.com>
2025-07-08 21:16:20 +0500

    feat: update allowed development origins in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev', 'busy-monster-needed.ngrok-free.app'],
 };
 
 export default nextConfig;
